0f6e44d776aaf43ccf59d144b48739d207bc0a1d
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2025-07-30 23:03:54 +0200

    projects ab caratules

---
+++ portfolio-nuriavaquero/components/project-card.js
@@ -0,0 +1,24 @@
+export function createProjectCard(project, category) {
+  const card = document.createElement("div");
+  card.classList.add("project-card");
+
+  card.innerHTML = `
+    <div class="image-wrapper">
+      <img src="${project.img || 'img/placeholder.jpg'}" alt="${project.title}" />
+     
+      <div class="overlay">
+<div class="overlay-text">
+  <p class="category">${category.toUpperCase()}</p>
+        <h4>${project.title}</h4>
+        <p class="short-desc">${project.description || ''}</p>
+        ${project.link ? `<a href="${project.link}" target="_blank" class="btn-more">MORE ABOUT</a>` : ""}
+    </div>
+      
+        </div>
+
+    </div>
+    <p class="project-title">${project.title}</p>
+  `;
+
+  return card;
+}
