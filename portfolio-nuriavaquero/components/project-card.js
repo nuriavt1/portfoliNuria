f01ff41cba83898e77767f735e4d48f523326566
nuriavt1 <134273029+nuriavt1@users.noreply.github.com>
2025-08-01 22:00:03 +0200

    creaNDO UNA PAGINA DETALLE

--- portfolio-nuriavaquero/components/project-card.js
+++ portfolio-nuriavaquero/components/project-card.js
@@ -5,20 +5,22 @@ export function createProjectCard(project, category) {
   card.innerHTML = `
     <div class="image-wrapper">
       <img src="${project.img || 'img/placeholder.jpg'}" alt="${project.title}" />
-     
       <div class="overlay">
-<div class="overlay-text">
-  <p class="category">${category.toUpperCase()}</p>
-        <h4>${project.title}</h4>
-        <p class="short-desc">${project.description || ''}</p>
-        ${project.link ? `<a href="${project.link}" target="_blank" class="btn-more">MORE ABOUT</a>` : ""}
-    </div>
-      
+        <div class="overlay-text">
+          <p class="category">${category.toUpperCase()}</p>
+          <h4>${project.title}</h4>
+          <p class="short-desc">${project.description || ''}</p>
+          ${project.link ? `<a href="${project.link}" target="_blank" class="btn-more">MORE ABOUT</a>` : ""}
         </div>
-
+      </div>
     </div>
     <p class="project-title">${project.title}</p>
   `;
+
+  // 👉 Evento de click para redirigir con el id
+  card.addEventListener("click", () => {
+    window.location.href = `detail.html?id=${project.id}`;
+  });
 
   return card;
 }
